98c741ad11eb2eac541bdbde9a1cc969327fcd2d
m23csa516 <m23csa516@iitj.ac.in>
2025-04-05 19:28:54 +0530

    Update Dental Vision.py

--- Dental Vision.py
+++ Dental Vision.py
@@ -258,7 +258,7 @@ def predict(resized_image):
     binary_mask = (pred_mask_image > 0.5).astype(np.uint8)
 
     print(binary_mask.shape)
-    prediction_image = binary_mask.reshape(mask.shape)
+    prediction_image = binary_mask.reshape(resized_image.shape)
     #print("PI:", prediction_image.shape)
     prediction_image_8 = (prediction_image * 255).astype(np.uint8)
     print(prediction_image_8.shape)
